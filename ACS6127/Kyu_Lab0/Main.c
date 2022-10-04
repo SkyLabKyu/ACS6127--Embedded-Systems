f4f850a297ce3cc1221db428ef0068ff3279cad5
KyuSiik <jamesacusick@gmail.com>
2022-10-04 16:09:17 +0100

    Lab 0 - Update 1
    Green LED now lights

--- ACS6127/Kyu_Lab0/Main.c
+++ ACS6127/Kyu_Lab0/Main.c
@@ -1,1 +1,9 @@
-void main() {};
+//Test program, makes LED blink on and off
+
+#include "stm32f4xx.h"
+
+void main() {
+	RCC->AHB1ENR |= RCC_AHB1ENR_GPIODEN; 	//Enable GPIO-D register
+	GPIOD->MODER |= GPIO_MODER_MODER12_0;		//Set GPIOD pin 12 to GP output
+	GPIOD->ODR = (1<<12); //Set pin12 high, to light LED
+};
---
+++ ACS6127/Kyu_Lab0/my_defines.d
@@ -0,0 +1,1 @@
+.\my_defines.o: my_defines.c
