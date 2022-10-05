c3f4012847e175ffa18c7f5a002c3f7061fd17eb
KyuSiik <jamesacusick@gmail.com>
2022-10-05 11:39:00 +0100

    Lab 0 - Complete
    LED blinks on and off with a reasonable period. This lab is finished until otherwise stated.

--- ACS6127/Kyu_Lab0/Main.c
+++ ACS6127/Kyu_Lab0/Main.c
@@ -3,7 +3,23 @@
 #include "stm32f4xx.h"
 
 void main() {
+	uint32_t ii = 0;
+	uint32_t thres = 8000000;
+	
 	RCC->AHB1ENR |= RCC_AHB1ENR_GPIODEN; 	//Enable GPIO-D register
 	GPIOD->MODER |= GPIO_MODER_MODER12_0;		//Set GPIOD pin 12 to GP output
-	GPIOD->ODR = (1<<12); //Set pin12 high, to light LED
-};
+	while(1){
+		
+		//Pin 12 high for 'thres' cycles
+		while(ii<thres){
+			GPIOD->ODR |= (1<<12); //Set pin12 high, to light LED
+			ii++;
+		}
+		
+		//Pin 12 low for 'thres' cycles
+		while(ii>0){
+			GPIOD->ODR &= !(1<<12); //Set pin12 low, disable LED
+			ii--;
+		}
+	}		
+}
